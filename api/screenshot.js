89e45f75b689dc46ea82baadfdb062df4ecbdd51
Xioteer <11512930+Xioteer@users.noreply.github.com>
2024-12-23 14:53:09 +0100

    Update screenshot.js

--- api/screenshot.js
+++ api/screenshot.js
@@ -9,16 +9,15 @@ module.exports = async (req, res) => {
             return res.status(400).json({ error: "URL is required" });
         }
 
-        // Launch browser using @sparticuz/chromium
         const browser = await puppeteer.launch({
-            executablePath: await chromium.executablePath(),
             args: chromium.args,
             defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath(),
             headless: chromium.headless,
+            ignoreHTTPSErrors: true,
         });
 
         const page = await browser.newPage();
-        await page.setViewport({ width: 1920, height: 1080 });
         await page.goto(url, { waitUntil: 'networkidle2' });
 
         const screenshot = await page.screenshot({ type: 'png' });
@@ -28,7 +27,7 @@ module.exports = async (req, res) => {
         res.setHeader('Content-Type', 'image/png');
         res.send(screenshot);
     } catch (error) {
-        console.error('Error while taking the screenshot:', error);
+        console.error('Error taking screenshot:', error);
         res.status(500).json({ error: "An error occurred while taking the screenshot" });
     }
 };
